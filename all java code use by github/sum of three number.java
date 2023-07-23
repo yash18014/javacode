0856b6b6c72bfd7c0ea6db5d687b6efc278ebb27
Darji Yash . S <111188806+yash18014@users.noreply.github.com>
2023-07-22 23:35:06 -0700

    Add files via upload

---
+++ all java code use by github/sum of three number.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int number1 = 6;
+        int number2 = 7;
+        int number3 = 8;
+
+        int sum = number1 + number2 + number3;
+
+        System.out.println(sum);
+    }
+}
